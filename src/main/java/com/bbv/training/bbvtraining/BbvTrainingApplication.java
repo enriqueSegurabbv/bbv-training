3f6f99d4fc024314cb778abadf656b6b10828c19
enriqueseguraquintana <segura.gcc@gmail.com>
2022-09-29 12:07:45 +0200

    [main] Initial commit.

---
+++ src/main/java/com/bbv/training/bbvtraining/BbvTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.bbv.training.bbvtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbvTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbvTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbv/training/bbvtraining/BbvTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbv.training.bbvtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbvTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
