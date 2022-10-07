960f38501c755d4fbcc281a91ffe53262dccb72e
enriqueseguraquintana <segura.gcc@gmail.com>
2022-10-07 14:35:44 +0200

    [main] Added delete controller method and getAll method.

--- src/main/java/com/bbv/training/bbvtraining/controller/ResourceClassController.java
+++ src/main/java/com/bbv/training/bbvtraining/controller/ResourceClassController.java
@@ -8,18 +8,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RequestMapping("/api/resourceclass")
 @RestController
 public class ResourceClassController {
 
     @Autowired
-    ResourceClassRepository repository;
+    private ResourceClassRepository repository;
 
+    @GetMapping("/all")
+    public List<ResourceClassEntity> getAll() {
+        return repository.findAll();
+    }
     @GetMapping("/{resourceclassId}")
     public ResourceClassEntity getResourceClass(@PathVariable("resourceclassId") Long id) {
         /// http://localhost:8080/api/resourceclass/12
         ResourceClassEntity result = repository.findById(id);
         return result;
+    }
+
+    @GetMapping("/delete/{resourceclassId}")
+    public String deleteResourceClassById(@PathVariable("resourceclassId") Long id) {
+        /// http://localhost:8080/api/resourceclass/12
+        Optional<ResourceClassEntity> result = Optional.ofNullable(repository.findById(id));
+        if (result.isPresent() == true) {
+            repository.deleteById(id);
+            return "<h2>Resource successfully deleted.</h2>";
+        }
+        return "<h2>No resource with id =  " + id + "</h2>";
     }
 
 }
