2266c5b4a0c68105fe90b49fe0e995f1258f5396
enriqueseguraquintana <segura.gcc@gmail.com>
2022-10-25 17:55:32 +0200

    [main] Changed component by Service and indented correctly.

--- src/main/java/com/bbv/training/bbvtraining/service/ResourceClassService.java
+++ src/main/java/com/bbv/training/bbvtraining/service/ResourceClassService.java
@@ -2,17 +2,21 @@ package com.bbv.training.bbvtraining.service;
 import com.bbv.training.bbvtraining.entity.ResourceClassEntity;
 import com.bbv.training.bbvtraining.repository.ResourceClassDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
-@Component
+
+@Service
 public class ResourceClassService {
+
     @Autowired
     private ResourceClassDataRepository repository;
+
     public ResourceClassEntity findById(Long id) {
+
         Optional<ResourceClassEntity> result = repository.findById(id);
+
         if(result.isPresent()) {
             return result.get();
         } else {
@@ -20,19 +24,28 @@ public class ResourceClassService {
         }
 
     }
+
     public List<ResourceClassEntity> findAll() {
         return repository.findAll();
     }
+
     public void deleteById(Long id) {
+
         Optional<ResourceClassEntity> result = repository.findById(id);
+
         if(result.isPresent()) {
             repository.delete(result.get());
         } else {
             throw new EntityNotFoundException("Entity with id "+id+" not found");
         }
+
     }
+
     public ResourceClassEntity save(ResourceClassEntity entity) {
+
         repository.save(entity);
+
         return entity;
+
     }
 }
